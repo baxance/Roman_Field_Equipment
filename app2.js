51cd4a14e8912bdf7eb8552ec4ec8e1920ede474
Barrett Nance <baxance@protonmail.com>
2021-01-06 18:36:43 -0800

    Created JS files and connected them to HTML

---
+++ app2.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Bonum Vesperam';
+} else if (hourNow > 12) {
+    greeting = 'Bona Dies';
+} else if (hourNow > 0) {
+    greeting = 'bonum Mane';
+} else {
+    greeting = 'Gratissium';
+}
+
+document.write('<h3>' + greeting + '</h3>');
---
+++ app3.js
@@ -0,0 +1,8 @@
+var today = new Date();
+var year = today.getFullYear();
+var est = new Date ('Sep 04, 0476');
+var difference = today.getTime() - est.getTime();
+difference = (difference / 31556900000);
+
+var elMsg = document.getElementById('message')
+elMsg.textContent = Math.floor(difference) + ' Years since the fall of the Western Empire';
