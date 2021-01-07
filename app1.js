2bf4adce9733fc36e723c70f476820e39f45bd7a
Barrett Nance <baxance@protonmail.com>
2021-01-06 16:23:49 -0800

    cleaned up some CSS and created .js files

---
+++ app1.js
@@ -0,0 +1,7 @@
+let userName = prompt('Please Enter Your Name');
+
+if (userName == 'Barrett'){
+    document.write('<h3>' + 'Salve Civis Barrett' + '</h3>');
+} else if (userName != 'Barrett'){
+    document.write('<h3>' + 'Salve Civis' + '</h3>')
+}
